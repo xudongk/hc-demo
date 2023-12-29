f3a92f0121b6343fc0d1d86348b4431240429328
xudongk <154393425+xudongk@users.noreply.github.com>
2023-12-29 14:41:04 +0800

    Create 006_create_table_test4.sql

---
+++ sql/006_create_table_test4.sql
@@ -0,0 +1,17 @@
+-- // create users table5
+-- Migration SQL that makes the change goes here.
+CREATE TABLE `test5` (
+  `id` int NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO test5 (id, name, age, email) VALUES(1, '1', 0, '2');
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+
+drop table test5;
