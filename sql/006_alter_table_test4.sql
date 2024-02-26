ec4877f6104fae1fa71f7bd3311cd6ccd08b6336
xudongk <154393425+xudongk@users.noreply.github.com>
2024-02-26 20:56:11 +0800

    Update 006_alter_table_test4.sql

--- sql/006_alter_table_test4.sql
+++ sql/006_alter_table_test4.sql
@@ -1,8 +1,8 @@
 -- // alter table test4
 -- Migration SQL that makes the change goes here.
 ALTER TABLE `test4` (
-  ADD telephone varchar(50)
-  DROP COLUMN age
+  ADD 'telephone' varchar(50)
+  DROP COLUMN 'age'
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
