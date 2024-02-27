7455379292a4dfa3f458e2dde0717c984c9062af
xudongk <154393425+xudongk@users.noreply.github.com>
2024-02-27 09:49:49 +0800

    Update 006_alter_table_test4.sql

--- sql/006_alter_table_test4.sql
+++ sql/006_alter_table_test4.sql
@@ -1,9 +1,6 @@
 -- // alter table test4
 -- Migration SQL that makes the change goes here.
-ALTER TABLE `test4` (
-  ADD 'telephone' varchar(50)
-  );
-
+ALTER TABLE `test4` ADD telephone varchar(50);
 
 INSERT INTO test4 (id, name, age, email,telephone) VALUES(10, '1', 0, '2','1234567');
 
