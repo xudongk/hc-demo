e9d8851e26ce645233db89f651a632eae0f0b021
xudongk <154393425+xudongk@users.noreply.github.com>
2024-02-27 09:27:22 +0800

    Update 006_alter_table_test4.sql

--- sql/006_alter_table_test4.sql
+++ sql/006_alter_table_test4.sql
@@ -1,11 +1,11 @@
 -- // alter table test4
 -- Migration SQL that makes the change goes here.
-ALTER TABLE `test4` 
+ALTER TABLE `test4` (
   ADD 'telephone' varchar(50)
-  DROP COLUMN 'age'
+  );
 
 
-INSERT INTO test4 (id, name, age, email) VALUES(10, '1', 0, '2');
+INSERT INTO test4 (id, name, age, email,telephone) VALUES(10, '1', 0, '2','1234567');
 
 -- //@UNDO
 -- SQL to undo the change goes here.
