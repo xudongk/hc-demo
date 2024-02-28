6bffd20f6247799dad73eb0f58f3890b4446c741
xudongk <154393425+xudongk@users.noreply.github.com>
2024-02-28 09:26:01 +0800

    Create 007_drop_table_test3.sql

---
+++ sql/007_drop_table_test3.sql
@@ -0,0 +1,16 @@
+-- // create users table
+-- Migration SQL that makes the change goes here.
+drop table test3;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+CREATE TABLE `test3` (
+  `id` int NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO test3 (id, name, age, email_test) VALUES(1, '1', 0, '2@qq.com');
